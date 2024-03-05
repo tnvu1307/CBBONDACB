dd0ceedaaa83c6986857711db54b4556e22b2c88
Trương Nhật Vy <vytruongjb@gmail.com>
2024-03-05 16:24:23 +0700

    build: update version 1018

--- CommonLibrary/AssemblyInfo.vb
+++ CommonLibrary/AssemblyInfo.vb
@@ -30,6 +30,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("1.0.1.7")>
+<Assembly: AssemblyVersion("1.0.1.8")>
 
-<Assembly: AssemblyFileVersion("1.0.1.7")>
+<Assembly: AssemblyFileVersion("1.0.1.8")>
